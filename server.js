93f9ded460841dfa4087ffe4fd78776846657e68
Daniel Winston <disabledfishinabarrel@gmail.com>
2024-03-31 15:53:23 -0600

    updated from HW3

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /*
-CSC3916 HW4
+CSC3916 HW2
 File: Server.js
 Description: Web API scaffolding for Movie API
  */
-
+var env = require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
 var passport = require('passport');
@@ -13,7 +13,6 @@ var jwt = require('jsonwebtoken');
 var cors = require('cors');
 var User = require('./Users');
 var Movie = require('./Movies');
-var Review = require('./Reviews');
 
 var app = express();
 app.use(cors());
@@ -44,7 +43,7 @@ function getJSONObjectForMovieRequirement(req) {
 
 router.post('/signup', function(req, res) {
     if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: 'Please include both username and password to signup.'})
+        res.json({success: false, msg: '/Please include both username and password to signup.'})
     } else {
         var user = new User();
         user.name = req.body.name;
@@ -53,6 +52,7 @@ router.post('/signup', function(req, res) {
 
         user.save(function(err){
             if (err) {
+                console.log(err.message);
                 if (err.code == 11000)
                     return res.json({ success: false, message: 'A user with that username already exists.'});
                 else
@@ -73,7 +73,6 @@ router.post('/signin', function (req, res) {
         if (err) {
             res.send(err);
         }
-
         user.comparePassword(userNew.password, function(isMatch) {
             if (isMatch) {
                 var userToken = { id: user.id, username: user.username };
@@ -86,9 +85,95 @@ router.post('/signin', function (req, res) {
         })
     })
 });
+router.route('/movies')
+    .get((req, res) => {
+        // Return all movies
+        Movie.find({}).exec(function(err, movies) {
+            if (err) {
+                console.log(err);
+            }
+
+            res.json ({success: true, movies: movies})
+        })
+    })
+    .post(authJwtController.isAuthenticated, (req, res) => {
+        Movie.find({title: req.body.title}).exec(function (err, found) {
+            if (err)
+                console.log(err);
+            // movie is found
+            if (found.length != 0)
+                return res.json({success: false, msg: 'Movie already exists.'});
+            else // movie is not found
+            {
+                // Save a single movie
+                var newMovie = new Movie();
+                newMovie.title = req.body.title;
+                newMovie.releaseDate = req.body.releaseDate;
+                newMovie.genre = req.body.genre;
+                newMovie.actors = req.body.actors;
+
+                if (newMovie.releaseDate < 1888 || newMovie.actors.length < 3 || !Movie.schema.path('genre').enumValues.includes(newMovie.genre))
+                    res.status(400).send({success: false, message: 'Unable to add film.'});
+                else
+                {
+                    newMovie.save(function(err){
+                        if (err) {
+                            console.log(err.message);
+                            return res.json(err);
+                        }
+    
+                        res.json({success: true, message: 'Successfully created new movie.'});
+                    });
+                }
+            }
+        })
+    })
+    .all((req, res) => {
+        res.status(405).send({message: 'HTTP method not supported.' });
+    })
+router.route('/movies/:movieparameter')
+    .get((req, res) => {
+        Movie.find({title: req.params.movieparameter}).exec(function(err, movie) {  
+            if (err)
+                console.log(err);
+            if (movie.length == 1)
+            {
+                title = movie[0].title;
+                res.json ({success: true, movie: movie});
+            }
+            else 
+            {
+                res.json({success: false});
+            }
+        })
+    })
+    .put(authJwtController.isAuthenticated, (req,res) => {
+        title = req.params.movieparameter;
+        // For now I am assuming that a partial match should overwrite the title of the film, only one argument is given to PUT /movies/nameTitle
+        Movie.updateOne({title: {$regex: title}}, {$set: {'title': title}}).exec(function(err, set) {
+            if (err)
+                console.log(err);
+            if (set.n == 1)
+                res.json ({success: true});
+            else
+                res.json ({success: false});
+        })
+    })
+    .delete(authJwtController.isAuthenticated, (req,res) => {
+        title = req.params.movieparameter;
+        Movie.deleteOne({title: title}).exec(function(err, rem) {
+            if (err)
+                console.log(err);
+            if (rem.n == 1)
+                res.json ({success: true});
+            else
+                res.json ({success: false});
+        })
+    })
+    .all((req, res) => {
+        res.status(405).send({message: 'HTTP method not supported.' });
+    })
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
 module.exports = app; // for testing only
-
-
